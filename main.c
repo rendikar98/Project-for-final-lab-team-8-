90c4cfa6b0fbbe74d3aa84936055229e63f9c964
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-05-16 22:54:19 +0700

    Add files via upload
    menambahkan file main temporary

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+//make function prototype, later make header file for all function
+void home_disp(int choice);
+int quiz(int *score);
+void sign_up(char *username, char *pass);
+void log_in(FILE *fptr, char *usernameInput, char *passwordInput);
 
+
+int main(int argc, char argv[])//argument for this program is username and pass
+{ 
+    // declaration for variable
+    // int choice;
+    // int sec;
+    // int score = 0;
+
+    //Signup & Login Area
+    printf("=======Signup Session===========\n");
+    printf("\tUsername:  \n");
+    printf("\tPassword:  \n");
+    printf("\tRe-confirm password:  \n");
+    //write to store user data to binary file
+    printf("=======Login Session==========");
+    printf("\tUsername:  \n");
+    printf("\tPassword:  \n");
+    //compare data from binary file with user input
+
+    //display main menu
+
+    //quiz area(question and score)
+    //display final score and details for user
+
+    return 0;
+}
