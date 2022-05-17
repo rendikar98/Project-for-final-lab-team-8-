d85753333893e0c139dd88ee21f826538dca5dd4
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-05-17 18:05:21 +0700

    menambahkan fungsi gotoxy untuk memindahkan kursor
    nanti rename fungsinya, fungsi ini untuk memindahkan kursor ke tempat yg diinginkan

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+COORD coord = {0,0};
+#define x 28
 //make function prototype, later make header file for all function
 void home_disp(int choice);
 int quiz(int *score);
 void sign_up(char *username, char *pass);
 void log_in(FILE *fptr, char *usernameInput, char *passwordInput);
+
+void gotoxy(int kolom, int baris)
+{
+    coord.X = kolom;
+    coord.Y = baris;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 
 int main(int argc, char argv[])//argument for this program is username and pass
@@ -15,14 +24,24 @@ int main(int argc, char argv[])//argument for this program is username and pass
     // int score = 0;
 
     //Signup & Login Area
+    system("cls");
+    gotoxy(x, 6);
     printf("=======Signup Session===========\n");
-    printf("\tUsername:  \n");
-    printf("\tPassword:  \n");
-    printf("\tRe-confirm password:  \n");
+    printf("\t\nUsername:  \n");
+    scanf("%d", &y);
+    printf("\t\nPassword:  \n");
+    scanf("%d", &y);
+    printf("\t\nRe-confirm password:  \n");
+    scanf("%d", &y);
+    system("cls");
     //write to store user data to binary file
-    printf("=======Login Session==========");
-    printf("\tUsername:  \n");
-    printf("\tPassword:  \n");
+    gotoxy(x, 12);
+    printf("=======Login Session==========\n");
+    printf("\t\nUsername:  \n");
+    scanf("%d", &y);
+    printf("\t\nPassword:  \n");
+    scanf("%d", &y);
+
     //compare data from binary file with user input
 
     //display main menu
