901082e823c022d63c66a1cc45034a8e9a3bb12c
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-05-19 22:35:31 +0700

    mengubah fungsi gotoxy dan merapihkan tampilan
    gotoxy diganti namanya menjadi cursor

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ void delay(int detik)
     while (clock() < start_time + milli_seconds);
 }
 
-void gotoxy(int kolom, int baris)
+void cursor(int kolom, int baris)
 {
     coord.X = kolom;
     coord.Y = baris;
@@ -29,126 +29,229 @@ void gotoxy(int kolom, int baris)
 
 void *soal(int *score,int *count){
   char pilihan1,pilihan2,pilihan3,pilihan4,pilihan5;
-  puts("Quiz dimulai");
-  delay(2);
-  puts("Soal Pertama");
-  delay(1);
-  puts("=======================================================================================");
-  printf("Manakah karakter disney yang terkenal meninggalkan sepatu kaca pada saat pesta dansa?\n");
-  puts("=======================================================================================");
-  puts("A. Pochatonas\t||\tB. Putri Tidur");
-  puts("C. Cinderella\t||\tD. Elsa");
-  puts("--------------------------------------");
-  printf("Pilihan Jawaban: ");
-  scanf("%c",&pilihan1);
-  delay(1);
-  if (toupper(pilihan1)=='C')
-  {
-      printf("Anda Benar\nPoin yang anda dapatkan sebesar 20");
-      *score +=20;
-      *count +=1;
-  }
-  else
-  {
-      printf("Anda Salah\nPoin yang anda dapatkan sebesar 0");
-  }
-  delay(1);
-  puts("\n=======================================================================================");
-  printf("---------------------------------Soal Selanjutnya--------------------------------------\n");
-  puts("=======================================================================================");
-  delay(1);
-  puts("==================================================================================================================================");
-  printf("Siapakah yang membuat marah Zinedine Zidane sehinnga Zidane melakukan tragedi penyundulan terhadapnya pada final Piala Dunia 2006?\n");
-  puts("==================================================================================================================================");
-  puts("A. Marco Materazzi\t||\tB. Fabio Cannavaro");
-  puts("C. Gianluigi Buffon\t||\tD. Filippo Inzaghi");
-  puts("--------------------------------------------------");
-  printf("Pilihan Jawaban: ");
-  scanf(" %c",&pilihan2);
-  delay(1);
-  if (toupper(pilihan2)=='A')
-  {
-      printf("Anda Benar\nPoin yang anda dapatkan sebesar 20");
-      *score +=20;
-      *count+=1;
-  }
-  else
-  {
-      printf("Anda Salah\nPoin yang anda dapatkan sebesar 0");
-  }
-  delay(1);
-  puts("\n=======================================================================================");
-  printf("---------------------------------Soal Selanjutnya--------------------------------------\n");
-  puts("=======================================================================================");
-  delay(1);
-  puts("=======================================================================================");
-  printf("Siapakah penemu bahasa C dalam pemograman?\n");
-  puts("=======================================================================================");
-  puts("A. Graydon Hoare\t||\tB. James Gosling");
-  puts("C. Guido Van Rossum\t||\tD. Dennis Ritchie");
-  puts("-------------------------------------------------");
-  printf("Pilihan Jawaban: ");
-  scanf(" %c",&pilihan3);
-  delay(1);
-  if (toupper(pilihan3)=='D')
-  {
-      printf("Anda Benar\nPoin yang anda dapatkan sebesar 20");
-      *score +=20;
-      *count+=1;
-  }
-  else
-  {
-      printf("Anda Salah\nPoin yang anda dapatkan sebesar 0");
-  }
-  delay(1);
-  puts("\n=======================================================================================");
-  printf("---------------------------------Soal Selanjutnya--------------------------------------\n");
-  puts("=======================================================================================");
-  delay(1);
-  puts("=======================================================================================");
-  printf("Manakah makanan yang tidak perlu menggunakan bumbu kacang?\n");
-  puts("=======================================================================================");
-  puts("A. Gado-gado\t||\tB. Rujak Ulek");
-  puts("C. Lumpia Basah\t||\tD. Sate");
-  puts("-------------------------------------");
-  printf("Pilihan Jawaban: ");
-  scanf(" %c",&pilihan4);
-  delay(1);
-  if (toupper(pilihan4)=='C')
-  {
-      printf("Anda Benar\nPoin yang anda dapatkan sebesar 20");
-      *score +=20;
-      *count+=1;
-  }
-  else
-  {
-      printf("Anda Salah\nPoin yang anda dapatkan sebesar 0");
-  }
-  delay(1);
-  puts("\n=======================================================================================");
-  printf("---------------------------------Soal Selanjutnya--------------------------------------\n");
-  puts("=======================================================================================");
-  delay(1);
-  puts("=======================================================================================");
-  printf("Siapakah pengarang dari serial manga berjudul One Piece?\n");
-  puts("=======================================================================================");
-  puts("A. Kazuki Takahashi\t||\tB. Saijyo Shinji");
-  puts("C. Tsunoda Jirou\t||\tD. Eiichiro Oda");
-  puts("------------------------------------------------");
-  printf("Pilihan Jawaban: ");
-  scanf(" %c",&pilihan5);
-  delay(1);
-  if (toupper(pilihan5)=='D')
-  {
-      printf("Anda Benar\nPoin yang anda dapatkan sebesar 20");
-      *score +=20;
-      *count+=1;
-  }
-  else
-  {
-      printf("Anda Salah\nPoin yang anda dapatkan sebesar 0");
-  }
-  delay(1);
+  system("cls");
+    cursor(36, 7);
+    printf("Quiz dimulai\n");
+    cursor(36, 8);
+    delay(2);
+    cursor(36, 8);
+    printf("========================================================================\n");
+    cursor(36, 9);
+    printf("----------------------------Soal Pertama--------------------------------\n");
+    cursor(36, 10);
+    printf("========================================================================\n");
+    delay(1);
+    cursor(36, 11);
+    printf("=======================================================================================\n");
+    cursor(36, 12);
+    printf("Manakah karakter disney yang terkenal meninggalkan sepatu kaca pada saat pesta dansa?\n");
+    cursor(36, 13);
+    printf("=======================================================================================\n");
+    cursor(36, 14);
+    printf("A. Pochatonas\t||\tB. Putri Tidur\n");
+    cursor(36, 15);
+    printf("C. Cinderella\t||\tD. Elsa\n");
+    cursor(36, 16);
+    printf("--------------------------------------\n");
+    cursor(36, 17);
+    printf("Pilihan Jawaban: ");
+    scanf("%c",&pilihan1);
+    delay(1);
+    cursor(36, 18);
+    if (toupper(pilihan1)=='C')
+    {
+        printf("Anda Benar\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 20\n");
+        *score +=20;
+        *count +=1;
+    }
+    else
+    {
+        printf("Anda Salah\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 0\n");
+    }
+
+    cursor(36, 8);
+    delay(1);
+    system("cls");
+    cursor(36, 8);
+    printf("========================================================================\n");
+    cursor(36, 9);
+    printf("----------------------------Soal Selanjutnya----------------------------\n");
+    cursor(36, 10);
+    printf("========================================================================\n");
+    delay(1);
+    cursor(36, 11);
+    printf("=============================================================================\n");
+    cursor(36, 12);
+    printf("Siapakah yang membuat marah Zinedine Zidane sehinnga Zidane melakukan tragedi\n");
+    cursor(36, 13);
+    printf("penyundulan terhadapnya pada final Piala Dunia 2006?\n");
+    cursor(36, 14);
+    printf("=============================================================================\n");
+    cursor(36, 15);
+    printf("A. Marco Materazzi\t||\tB. Fabio Cannavaro\n");
+    cursor(36, 16);
+    printf("C. Gianluigi Buffon\t||\tD. Filippo Inzaghi\n");
+    cursor(36, 17);
+    printf("--------------------------------------------------\n");
+    cursor(36, 18);
+    printf("Pilihan Jawaban: ");
+    scanf(" %c",&pilihan2);
+    delay(1);
+    cursor(36, 19);
+    if (toupper(pilihan2)=='A')
+    {
+        printf("Anda Benar\n");
+        cursor(36, 20);
+        printf("Poin yang anda dapatkan sebesar 20\n");
+        *score +=20;
+        *count +=1;
+    }
+    else
+    {
+        printf("Anda Salah\n");
+        cursor(36, 20);
+        printf("Poin yang anda dapatkan sebesar 0\n");
+        *score +=20;
+        *count +=1;
+    }
+
+    cursor(36, 8);
+    delay(1);
+    system("cls");
+    cursor(36, 8);
+    printf("========================================================================\n");
+    cursor(36, 9);
+    printf("----------------------------Soal Selanjutnya----------------------------\n");
+    cursor(36, 10);
+    printf("========================================================================\n");
+    delay(1);
+    cursor(36, 11);
+    printf("========================================================================\n");
+    cursor(36, 12);
+    printf("Siapakah penemu bahasa C dalam pemograman?\n");
+    cursor(36, 13);
+    printf("========================================================================\n");
+    cursor(36, 14);
+    printf("A. Graydon Hoare\t||\tB. James Gosling\n");
+    cursor(36, 15);
+    printf("C. Guido Van Rossum\t||\tD. Dennis Ritchie\n");
+    cursor(36, 16);
+    printf("-------------------------------------------------\n");
+    cursor(36, 17);
+    printf("Pilihan Jawaban: ");
+    scanf(" %c",&pilihan3);
+    delay(1);
+    cursor(36, 18);
+    if (toupper(pilihan3)=='D')
+    {
+        printf("Anda Benar\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 20\n");
+        *score +=20;
+        *count +=1;
+    }
+    else
+    {
+        printf("Anda Salah\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 0\n");
+        *score +=20;
+        *count +=1;
+    }
+
+    cursor(36, 8);
+    delay(1);
+    system("cls");
+    cursor(36, 8);
+    printf("========================================================================\n");
+    cursor(36, 9);
+    printf("----------------------------Soal Selanjutnya----------------------------\n");
+    cursor(36, 10);
+    printf("========================================================================\n");
+    delay(1);
+    cursor(36, 11);
+    printf("========================================================================\n");
+    cursor(36, 12);
+    printf("Manakah makanan yang tidak perlu menggunakan bumbu kacang?\n");
+    cursor(36, 13);
+    printf("========================================================================\n");
+    cursor(36, 14);
+    printf("A. Gado-gado\t||\tB. Rujak Ulek\n");
+    cursor(36, 15);
+    printf("C. Lumpia Basah\t||\tD. Sate\n");
+    cursor(36, 16);
+    printf("-------------------------------------\n");
+    cursor(36, 17);
+    printf("Pilihan Jawaban: ");
+    scanf(" %c",&pilihan4);
+    delay(1);
+    cursor(36, 18);
+    if (toupper(pilihan4)=='C')
+    {
+        printf("Anda Benar\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 20\n");
+        *score +=20;
+        *count +=1;
+    }
+    else
+    {
+        printf("Anda Salah\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 0\n");
+        *score +=20;
+        *count +=1;
+    }
+
+    cursor(36, 8);
+    delay(1);
+    system("cls");
+    cursor(36, 8);
+    printf("========================================================================\n");
+    cursor(36, 9);
+    printf("----------------------------Soal Selanjutnya----------------------------\n");
+    cursor(36, 10);
+    printf("========================================================================\n");
+    delay(1);
+    cursor(36, 11);
+    printf("========================================================================\n");
+    cursor(36, 12);
+    printf("Siapakah pengarang dari serial manga berjudul One Piece?\n");
+    cursor(36, 13);
+    printf("========================================================================\n");
+    cursor(36, 14);
+    printf("A. Kazuki Takahashi\t||\tB. Saijyo Shinji\n");
+    cursor(36, 15);
+    printf("C. Tsunoda Jirou\t||\tD. Eiichiro Oda\n");
+    cursor(36, 16);
+    printf("------------------------------------------------\n");
+    cursor(36, 17);
+    printf("Pilihan Jawaban: ");
+    scanf(" %c",&pilihan5);
+    delay(1);
+    cursor(36, 18);
+    if (toupper(pilihan5)=='D')
+    {
+        printf("Anda Benar\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 20\n");
+        *score +=20;
+        *count +=1;
+    }
+    else
+    {
+        printf("Anda Salah\n");
+        cursor(36, 19);
+        printf("Poin yang anda dapatkan sebesar 0\n");
+        *score +=20;
+        *count +=1;
+    }
+    delay(1);
+    return 0;
 }
 
 int main(int argc, char argv[])//argument for this program is username and pass
@@ -207,9 +310,11 @@ int main(int argc, char argv[])//argument for this program is username and pass
     //quiz area(question and score)
     int score=0,count=0;
     soal(&score,&count);
-    printf("\n%d",count);
-    printf("\n%d",score);
-    //display final score and details for user
-
+    system("cls");
+    cursor(36, 8);
+    printf("Soal yang benar %d\n",count);
+    cursor(36, 10);
+    printf("Total skor anda adalah %d\n",score);
+    
     return 0;
 }
