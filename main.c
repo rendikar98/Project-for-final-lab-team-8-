bf57459af1c00b1f97562344ea735c3df13b977b
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-05-19 16:29:24 +0700

    menghapus komen yang tidak diperlukan lagi

--- main.c
+++ main.c
@@ -5,10 +5,6 @@
 COORD coord = {0,0};
 #define x 28
 //make function prototype, later make header file for all function
-void home_disp(int choice);
-int quiz(int *score);
-void sign_up(char *username, char *pass);
-void log_in(FILE *fptr, char *usernameInput, char *passwordInput);
 
 void gotoxy(int kolom, int baris)
 {
@@ -20,11 +16,6 @@ void gotoxy(int kolom, int baris)
 
 int main(int argc, char argv[])//argument for this program is username and pass
 { 
-    // declaration for variable
-    // int choice;
-    // int sec;
-    // int score = 0;
-    
     // Login
     
     // Jika argumen yang diinput user kurang atau lebih dari 3 maka akses ditolak dan keluar dari program
@@ -74,28 +65,6 @@ int main(int argc, char argv[])//argument for this program is username and pass
 
         return EXIT_FAILURE;
     }
-
-    //Signup 
-    system("cls");
-    gotoxy(x, 6);
-    printf("=======Signup Session===========\n");
-    printf("\t\nUsername:  \n");
-    scanf("%d", &y);
-    printf("\t\nPassword:  \n");
-    scanf("%d", &y);
-    printf("\t\nRe-confirm password:  \n");
-    scanf("%d", &y);
-    system("cls");
-    //write to store user data to binary file
-    gotoxy(x, 12);
-    printf("=======Login Session==========\n");
-    printf("\t\nUsername:  \n");
-    scanf("%d", &y);
-    printf("\t\nPassword:  \n");
-    scanf("%d", &y);
-
-    //compare data from binary file with user input
-
     //display main menu
 
     //quiz area(question and score)
